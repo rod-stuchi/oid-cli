4751f144e92ea91b831b9ae4821731903e391f6f
Rodrigo Stuchi <rod.stuchi@gmail.com>
2018-04-02 06:45:08 -0300

    fix: cli argument, not set value

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const bsonObjectId = require('bson-objectid');
   const args = process.argv;
   if (/^-n\d*$/.test(args[2])) {
     let num;
-    num = parseInt((args[2].match(/\d+/) || [0])[0], 10);
+    num = parseInt(((args[2] || '0').match(/\d+/) || [0])[0], 10);
     if (!num) {
-      num = parseInt((args[3].match(/\d+/) || [0])[0], 10);
+      num = parseInt(((args[3] || '1').match(/\d+/) || [1]), 10);
     }
 
     for (var i = 0; i < num; i++) {
